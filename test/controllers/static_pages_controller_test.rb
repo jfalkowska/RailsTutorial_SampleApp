a095bc35c1363db5d4149a8f9e1ebc3b84d703fc
jfalkowska <jfalkowska@gmail.com>
2018-07-07 18:06:05 +0200

    Embedded Ruby - refactor

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup do
+  def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
